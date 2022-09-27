278d6ee18b6160fa8465819e4fdfbbf0db91d89b
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-26 20:33:45 -0600

    feat: completed user story 1, added within blocks and proper show pathing

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -13,17 +13,6 @@ RSpec.describe 'Discount Index Page' do
     @discount_2 = create(:discount, merchant: @merchant_1)
     # @discount_3 = create(:discount, merchant: @merchant_2)
   end
-      # Merchant Bulk Discounts Index
-
-      # As a merchant
-      # When I visit my merchant dashboard
-      # Then I see a link to view all my discounts
-      # When I click this link
-
-      # Then I am taken to my bulk discounts index page
-      # Where I see all of my bulk discounts including their
-      # percentage discount and quantity thresholds
-      # And each bulk discount listed includes a link to its show page
 
   it 'I see a link to view all my discounts, that takes me to my bulk index page - with all bulk discounts their % discount and quantity threshold, with a link to its show page' do
     visit merchant_dashboard_path(@merchant_1)
@@ -34,8 +23,9 @@ RSpec.describe 'Discount Index Page' do
     expect(page).to have_content(@discount_1.discount_amount.round(2) * 100)
     expect(page).to have_content(@discount_1.threshold)
     
-    save_and_open_page
-    click_link "Discount Description"
-    expect(current_path).to eq(merchant_discount_path(@discount_1)) #discount show
+    within "#discount-#{@discount_1.id}" do
+      click_link "Discount Description"
+      expect(current_path).to eq(merchant_discount_path(@merchant_1, @discount_1)) #discount show
+    end
   end
 end
