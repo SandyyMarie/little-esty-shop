e5414c2e9a659bcbb3cb0936e4c9af0df3ab2d3f
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-27 01:04:22 -0600

    feat: completed US4 preemtively during US1

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -3,24 +3,18 @@ require 'rails_helper'
 RSpec.describe 'visiting the Discount Show Page' do
   before :each do
     @merchant_1 = create(:merchant)
-    @merchant_2 = create(:merchant)
-    
-    @items_1 = create_list(:item, 10, merchant: @merchant_1)
-    @items_1 = create_list(:item, 20, merchant: @merchant_1)
-    
-    
+
     @discount_1 = create(:discount, merchant: @merchant_1)
     @discount_2 = create(:discount, merchant: @merchant_1)
-    @discount_3 = create(:discount, merchant: @merchant_2)
   end
   # As a merchant
   # When I visit my bulk discount show page
   # Then I see the bulk discount's quantity threshold and percentage discount
 
-  it 'shows the bulk discounts quantitity threshold and percentage discount' do
-    visit merchant_discount_path(@merchant_1, @discount_1) #show
+  it 'shows the bulk discounts quantitity threshold and percentage discount (US#4)' do
+    visit merchant_discount_path(@merchant_1, @discount_2) #show
 
-    expect(page).to have_content(@discount_1.discount_amount.round(2) * 100)
-    expect(page).to have_content(@discount_1.threshold)
+    expect(page).to have_content(@discount_2.discount_amount.round(2) * 100)
+    expect(page).to have_content(@discount_2.threshold)
   end
 end
