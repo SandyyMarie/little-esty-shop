3ea35ad7ed94924aa9f929db5443f72c2f9da2fa
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-26 21:20:12 -0600

    test: added testing for US#2

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Discount Index Page' do
     # @discount_3 = create(:discount, merchant: @merchant_2)
   end
 
-  it 'I see a link to view all my discounts, that takes me to my bulk index page - with all bulk discounts their % discount and quantity threshold, with a link to its show page' do
+  it 'From dashboard I see a link to view all my discounts, that takes me to my bulk index page - with all bulk discounts their % discount and quantity threshold, with a link to its show page (US#1)' do
     visit merchant_dashboard_path(@merchant_1)
 
     click_link "View all discounts"
@@ -27,5 +27,25 @@ RSpec.describe 'Discount Index Page' do
       click_link "Discount Description"
       expect(current_path).to eq(merchant_discount_path(@merchant_1, @discount_1)) #discount show
     end
+  end
+
+  it 'when visiting discount index page I see a link to create a new discount, which takes me to a new page (US#2)' do
+    visit merchant_discounts_path(@merchant_1) #discount index
+
+    click_link "Create new discount"
+
+    expect(current_path).to eq(new_merchant_discount_path(@merchant_1))
+  end
+
+  it 'the new page from the new discount has a form that you fill with valid data and then are redirected back to discount#index and see new discount listed (US#2)' do
+    visit new_merchant_discount_path(@merchant_1) #discount new
+
+    fill_in "Discount Amount:", with: 0.25
+    fill_in "Threshold:", with: 17
+    click_button "Save new discount"
+
+    expect(current_path).to eq(merchant_discounts_path(@merchant_1))
+    expect(page).to have_content("Discount Amount: 25%")
+    expect(page).to have_content("Threshold Quantity: 17")
   end
 end
